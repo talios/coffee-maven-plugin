8552a9c39764c1abf284fc12fd11816d3efe99c4
Daniel Bower <daniel.bower@ecommerce.com>
2011-10-12 12:03:08 -0400

    add license found elsewhere in project, extract out compiler version to var, add comment

--- src/main/java/com/theoryinpractise/coffeescript/CoffeeScriptCompiler.java
+++ src/main/java/com/theoryinpractise/coffeescript/CoffeeScriptCompiler.java
@@ -1,5 +1,21 @@
 package com.theoryinpractise.coffeescript;
 
+/*
+ * Copyright 2001-2005 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import com.google.common.base.Charsets;
 import com.google.common.io.InputSupplier;
 import com.google.common.io.Resources;
@@ -10,15 +26,21 @@ import org.mozilla.javascript.Scriptable;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+/**
+ * Wrapper around the coffee-script compiler from https://github.com/jashkenas/coffee-script/
+ *
+ */
 public class CoffeeScriptCompiler {
 
+	private String coffeeScriptCompilerScript = "coffee-script-1.1.2.js";
+	
     private final Scriptable globalScope;
     private boolean bare;
 
     public CoffeeScriptCompiler(boolean bare) {
 
         this.bare = bare;
-        InputSupplier<InputStreamReader> supplier = Resources.newReaderSupplier(getClass().getResource("/coffee-script-1.1.2.js"), Charsets.UTF_8);
+        InputSupplier<InputStreamReader> supplier = Resources.newReaderSupplier(getClass().getResource("/" + coffeeScriptCompilerScript), Charsets.UTF_8);
         Context context = Context.enter();
         context.setOptimizationLevel(-1); // Without this, Rhino hits a 64K bytecode limit and fails
         try {
