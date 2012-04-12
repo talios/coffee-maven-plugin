85f7370c924dd00a6b443309153784da9a1df725
Mark Derricutt <mark@talios.com>
2012-04-12 23:37:05 +1200

    Use sandboxed param

--- src/main/java/com/theoryinpractise/coffeescript/CoffeeScriptCompiler.java
+++ src/main/java/com/theoryinpractise/coffeescript/CoffeeScriptCompiler.java
@@ -95,15 +95,11 @@ public class CoffeeScriptCompiler {
         return context;
     }
 
-    private Require getSandboxedRequire(final Context cx) throws URISyntaxException {
-        return getSandboxedRequire(cx, cx.initStandardObjects(), true);
-    }
-
     private Require getSandboxedRequire(Context cx, Scriptable scope, boolean sandboxed) throws URISyntaxException {
         return new Require(cx, cx.initStandardObjects(),
                 new StrongCachingModuleScriptProvider(
                         new UrlModuleSourceProvider(Collections.singleton(
-                                getDirectory()), null)), null, null, true);
+                                getDirectory()), null)), null, null, sandboxed);
     }
 
     private URI getDirectory() throws URISyntaxException {
